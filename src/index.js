b78af995e44d04c61c15dd84510d55d7447f9c7d
acciarri93 <acciarri93@gmail.com>
2022-10-11 13:31:39 -0300

    cambios nuevos

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const messages = ["Oscar", "Ana", "Nikolai", "Diego", "Laura"];
 
 const randomMsg = () => {
-  const message = messages[Math.floor(Math.random() * mesagges.length)];
-  console.log(mesagge);
+  const message = messages[Math.floor(Math.random() * messages.length)];
+  console.log(message);
 };
+
+console.log(randomMsg);
 
 module.exports = { randomMsg };
